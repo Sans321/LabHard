49e49ff633eff2ab220ac21fe187db637ecf0405
Svita Artem <hacker2019af@bk.ru>
2021-06-10 22:08:55 +0500

    Lab 2

---
+++ LabHard(Svita)-1191-2/Task2_3/Objects/main.d
@@ -0,0 +1,11 @@
+./objects/main.o: main.c main.h \
+  D:\MDK\Packed\Keil\STM32F0xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f0xx.h \
+  D:\MDK\Packed\Keil\STM32F0xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f072xb.h \
+  D:\MDK\Packed\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm0.h \
+  D:\MDK\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\MDK\Packed\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  D:\MDK\Packed\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  D:\MDK\Packed\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  D:\MDK\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  D:\MDK\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  D:\MDK\Packed\Keil\STM32F0xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\system_stm32f0xx.h
